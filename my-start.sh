4c81b1919da3f20f49e51dccf6725b6ceaeb0736
Topher Sheridan <topher@topher.au>
2026-09-24 03:48:29 +1000

    Change repo to reduce time

--- my-start.sh
+++ my-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Symlink the model snapshot
 if [[ ! -e "/runpod-volume/models" ]]; then
-	CACHE_BASE="/runpod-volume/huggingface-cache/hub/models--Comfy-Org--Qwen-Image-2.1/snapshots"
+	CACHE_BASE="/runpod-volume/huggingface-cache/hub/models--TopherAU--Qwen-Image-2.1/snapshots"
 	SNAPSHOT_PATH="$(ls -d "${CACHE_BASE}/*" | head -n 1)"
 	ln -s "${SNAPSHOT_PATH}" "/runpod-volume/models"
 fi
